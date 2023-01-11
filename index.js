ee3fed29fac2c23b530f8fae77b5c232ca542d75
MiguelCriollo <macriollo.2@sudamericano.edu.ec>
2023-01-11 11:53:26 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+console.log("holaaaaaaa");
+let country;
+country="Ecuador";
+alert (country);
+let age;
+age= 18
+console.log(age);
---
+++ tipos.js
@@ -0,0 +1,10 @@
+let name="Miguel Angel";
+let lastname="Criollo Vasquez";
+let age=18;
+let student=true;
+let sports=["Voley","Football"];
+let hasCovid=false;
+let birthday="15 mayo";
+let gender="male";
+console.log(name,lastname,age,student,sports,hasCovid,birthday,gender);
+
